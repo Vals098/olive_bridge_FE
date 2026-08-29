cc13ad4d83703ae2ef10d876168c9d0521979c47
Vals098 <valeria.farinosi@gmail.com>
2026-08-29 18:50:31 +0200

    feat: set up Redux provider and app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import "./App.css";
+
+import store from "./redux/store";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 
-function App() {
+const MainLayout = () => {
+    return (
+        <>
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </>
+    );
+};
+
+function AppContent() {
     return (
         <BrowserRouter>
-        <Navbar/>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/products" element={<Products />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+
+                <Route element={<MainLayout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/products" element={<Products />} />
+                </Route>
             </Routes>
-            <Footer/>
         </BrowserRouter>
     );
 }
 
-export default App;
+export default function App() {
+    return (
+        <Provider store={store}>
+            <AppContent />
+        </Provider>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
-import { createRoot } from "react-dom/client"
-import "./index.css"
-import App from "./App.tsx"
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(<App />)
+createRoot(document.getElementById("root")!).render(
+    <App />
+);
