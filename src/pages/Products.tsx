29e912cf9ab53450ab394a787ffd4ef02d291758
Vals098 <valeria.farinosi@gmail.com>
2026-09-03 18:17:09 +0200

    feat: connect products page to Redux
    connection test positive

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,5 +1,22 @@
-function Products(){
-    return <h1>Products</h1>
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import type { AppDispatch, RootState } from "../redux/store";
+import { getProducts } from "../redux/actions/productAction/getProducts";
+
+function Products() {
+    const dispatch = useDispatch<AppDispatch>();
+
+    const products = useSelector(
+        (state: RootState) => state.product.products
+    );
+
+    useEffect(() => {
+        dispatch(getProducts());
+    }, [dispatch]);
+
+    console.log(products);
+
+    return <h1>Products</h1>;
 }
 
-export default Products
+export default Products;
