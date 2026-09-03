70ceb3f53f0bb31d20fffc3c4d74cf6a63f41a30
Vals098 <valeria.farinosi@gmail.com>
2026-09-03 23:34:18 +0200

    feat: fetch product detail

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,12 +1,41 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import type { Product } from "../types/Product";
 
 function ProductDetail() {
     const { productId } = useParams();
+    const [product, setProduct] = useState<Product | null>(null);
+
+    useEffect(() => {
+        const getProduct = async () => {
+            try {
+                const response = await fetch(
+                    `http://localhost:8080/products/${productId}`
+                );
+
+                if (!response.ok) {
+                    throw new Error("Unable to retrieve product");
+                }
+
+                const product: Product = await response.json();
+                setProduct(product);
+            } catch (error) {
+                console.error("Product retrieval error:", error);
+            }
+        };
+
+        getProduct();
+    }, [productId]);
+
+    if (!product) {
+        return <p>Loading...</p>;
+    }
 
     return (
         <div>
-            <h1>Product detail</h1>
-            <p>Product ID: {productId}</p>
+            <h1>{product.name}</h1>
+            <img src={product.image} alt={product.name} />
+            <p>{product.description}</p>
         </div>
     );
 }
