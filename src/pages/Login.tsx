021fb1b7ba8776ea3b3ff55036b3016fcb670e3a
Vals098 <valeria.farinosi@gmail.com>
2026-09-03 16:51:33 +0200

    feat: add automatic navigation after login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,52 +1,56 @@
-import { useState, type SubmitEvent } from "react";
-import { useDispatch } from "react-redux";
-import type { AppDispatch } from "../redux/store";
-import { loginAction } from "../redux/actions/userAction/login";
+import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import type { AppDispatch } from "../redux/store"
+import { loginAction } from "../redux/actions/userAction/login"
 
 function Login() {
-    const dispatch = useDispatch<AppDispatch>();
+  const dispatch = useDispatch<AppDispatch>()
+  const navigate = useNavigate()
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
-    const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
-        e.preventDefault();
+  const handleSubmit = async (e: React.SubmitEvent) => {
+    e.preventDefault()
 
-        dispatch(
-            loginAction({
-                email,
-                password,
-            })
-        );
-    };
+    await dispatch(
+      loginAction({
+        email,
+        password,
+      }),
+    )
 
-    return (
+    navigate("/")
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
         <div>
-            <h1>Login</h1>
-
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Email</label>
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
-
-                <div>
-                    <label>Password</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-
-                <button type="submit">Login</button>
-            </form>
+          <label>Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-    );
+
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
 }
 
-export default Login;
+export default Login
