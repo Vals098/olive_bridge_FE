b1092d1872d32f08ccb9f3538ce6bd1564389ff8
Vals098 <valeria.farinosi@gmail.com>
2026-09-03 22:44:57 +0200

    style: refine product card appearance

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,38 +1,32 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Container, Row, Col } from "react-bootstrap";
-import type { AppDispatch, RootState } from "../redux/store";
-import { getProducts } from "../redux/actions/productAction/getProducts";
-import ProductCard from "../components/ProductCard";
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Container, Row, Col } from "react-bootstrap"
+import type { AppDispatch, RootState } from "../redux/store"
+import { getProducts } from "../redux/actions/productAction/getProducts"
+import ProductCard from "../components/ProductCard"
 
 function Products() {
-    const dispatch = useDispatch<AppDispatch>();
+  const dispatch = useDispatch<AppDispatch>()
 
-    const products = useSelector(
-        (state: RootState) => state.product.products
-    );
+  const products = useSelector((state: RootState) => state.product.products)
 
-    useEffect(() => {
-        dispatch(getProducts());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getProducts())
+  }, [dispatch])
 
-    return (
-        <Container className="products-page">
-            <h1>Products</h1>
+  return (
+    <Container className="products-page">
+      <h1>Products</h1>
 
-            <Row className="products-grid">
-                {products.map((product) => (
-                    <Col
-                        key={product.productId}
-                        md={4}
-                        lg={3}
-                    >
-                        <ProductCard product={product} />
-                    </Col>
-                ))}
-            </Row>
-        </Container>
-    );
+      <Row className="products-grid">
+        {products.map((product) => (
+          <Col key={product.productId} md={4} lg={3} className="d-flex">
+            <ProductCard product={product} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  )
 }
 
-export default Products;
+export default Products
